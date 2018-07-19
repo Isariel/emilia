96f15842ed300bbb9207e858bc6ea1278efb7ba9
Felix Klauke <info@felix-klauke.de>
2018-07-19 13:39:59 +0200

    Add spigot dependencies and add plugin main class with corresponding plugin.yml.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

---
+++ src/main/kotlin/de/d3adspace/emilia/plugin/EmiliaPlugin.kt
@@ -0,0 +1,23 @@
+package de.d3adspace.emilia.plugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+/**
+ * Central spigot plugin class.
+ *
+ * @author Felix Klauke <info@felix-klauke.de>
+ */
+class EmiliaPlugin : JavaPlugin() {
+
+    override fun onLoad() {
+
+    }
+
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+
+    }
+}
